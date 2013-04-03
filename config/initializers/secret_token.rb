69641f5a65e71ec46d01db135ed3680c58978c4a
Stas <stanislav.belkov@gmail.com>
2013-04-03 12:39:01 +1100

    Basic setup of mongodb complete.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Luxhotels::Application.config.secret_token = '7b65ddd7c642569ffb4552c2e7d65874af253345a6cfab94f2b30c64a3c42d08b614af01dee110d042d3a3f79d697f3a2d2554dfc3c5ba0475774ee2165c52a3'
