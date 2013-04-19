46c47080203c89d07368768028144eae2646c0d3
Stas <stanislav.belkov@gmail.com>
2013-04-19 15:30:55 +1000

    Hotel data is now being imported from Expedia, and deposited in the remote Mongo db, and S3.

--- app/controllers/hotels_controller.rb
+++ app/controllers/hotels_controller.rb
@@ -1,3 +1,6 @@
+require 'json'
+require 'open-uri'
+
 class HotelsController < ApplicationController
   include SearchHotel
 
@@ -20,8 +23,29 @@ class HotelsController < ApplicationController
 
   def index
     @hotels = Hotel.all
-    @api_hotels = GetHotels({:customerIpAddress => "60.241.64.60", :customerUserAgent => "Mozilla/5.0 (Windows; U; Windows NT 5.0; en-US; rv:1.4b) Gecko/20030516 Mozilla Firebird/0.6", :city => "Bangkok", :countryCode => "TH", :numberOfResults => "10", :minStarRating => "4.0"})
-    @response_body = @api_hotels.body
+
+    api = Expedia::Api.new
+
+    api_hotels = api.get_list({:customerIpAddress => "60.241.64.60",\
+                             :customerUserAgent => "Mozilla/5.0 (Windows; U; Windows NT 5.0; en-US; rv:1.4b)\
+                             Gecko/20030516 Mozilla Firebird/0.6",\
+                             :city => "Bangkok", :countryCode => "TH", :numberOfResults => "50", :minStarRating => "4.0"})
+
+    @api_hotels_hash = api_hotels.body
+
+    hotel_objects = api_hotels.body['HotelListResponse']['HotelList']['HotelSummary']
+
+    hotel_objects.first(5).each do |f|
+      existing_hotels = Hotel.where(hotelId: f.fetch("hotelId")).count
+      if existing_hotels == 0
+        existing_thumb = f["thumbNailUrl"].to_s
+        large_image_url = "http://images.travelnow.com" + existing_thumb.sub("_t.jpg","_b.jpg")
+        @hotel = Hotel.create(f)
+        @hotel.remote_image_url = large_image_url
+        @hotel.save
+      end
+    end
+
   end
 
 end
--- app/models/hotel.rb
+++ app/models/hotel.rb
@@ -1,9 +1,31 @@
 class Hotel
   include Mongoid::Document
-  field :hotel_id, type: Integer
+  field :hotelId, type: Integer
   field :name, type: String
-  field :address, type: String
+  field :address1, type: String
+  field :address2, type: String
   field :city, type: String
-  field :country_code, type: Integer
+  field :stateProvinceCode, type: String
+  field :countryCode, type: String
+  field :postalCode, type: String
+  field :airportCode, type: String
+  field :supplierType, type: String
+  field :propertyCategory, type: String
+  field :hotelRating, type: Float
+  field :confidenceRating, type: Integer
+  field :amenityMask, type: Integer
+  field :shortDescription, type: String
+  field :locationDescription, type: String
+  field :lowRate, type: String
+  field :highRate, type: String
+  field :rateCurrencyCode, type: String
+  field :latitude, type: Float
+  field :longitude, type: Float
+  field :proximityDistance, type: Float
+  field :proximityUnit, type: Float
+  field :hotelDestination, type: Boolean
+  field :thumbNailUrl, type: String
+  field :deepLink, type: String
+
   mount_uploader :image, HotelImageUploader
 end
