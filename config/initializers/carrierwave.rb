e1325bb3a1c891b15fff4f3e050b0f1fba7c2a6a
Stas Belkov <belkiii@gmail.com>
2014-03-13 00:30:27 +1100

    Update carrierwave.rb

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,8 +1,6 @@
 CarrierWave.configure do |config|
   config.fog_credentials = {
     :provider               => 'AWS',
-    :aws_access_key_id      => 'AKIAI5X23BNMCXTKLYQA',
-    :aws_secret_access_key  => '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',
     :region                 => 'ap-southeast-2'
   }
   config.fog_directory  = 'lxhotels'
